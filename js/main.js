e525cb0458bc5cc441e5cb4ed532b44080d60193
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-05-15 22:12:46 +0600

    make slider jQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(function(){
-
+$(function () {
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
+    });
 });
